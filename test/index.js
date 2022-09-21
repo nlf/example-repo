6211a04084e67061450360c66ed0572cec8f69de
nlf <quitlahok@gmail.com>
2022-09-21 11:12:50 -0700

    chore: remove failing tests

--- test/index.js
+++ test/index.js
@@ -2,14 +2,14 @@ const t = require('tap')
 
 t.pass('tap ran')
 
-t.test('matcher test', t => {
-  t.fail('kerplooey')
-  t.end()
-})
-
-t.test('another matcher', t => {
-  t.equal(true, false, 'expected true to be false')
-
-  t.same({ a: true }, { b: false }, 'same objects? not quite')
-  t.end()
-})
+// t.test('matcher test', t => {
+//   t.fail('kerplooey')
+//   t.end()
+// })
+//
+// t.test('another matcher', t => {
+//   t.equal(true, false, 'expected true to be false')
+//
+//   t.same({ a: true }, { b: false }, 'same objects? not quite')
+//   t.end()
+// })
